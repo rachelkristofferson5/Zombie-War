f2599ab706c4d599311d543e7f51efab9d164160
rachelkristofferson5 <kristofr@csp.edu>
2025-07-29 09:46:27 -0500

    Initialized project classes

---
+++ src/main/java/com/zombiewar/ZombieWar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.zombiewar;
+
+/**
+ *
+ * @author rachel
+ */
+public class ZombieWar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
