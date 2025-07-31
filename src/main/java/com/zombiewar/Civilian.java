fb3dd12c184aaf29041249de5012638e24c6f429
rachelkristofferson5 <kristofr@csp.edu>
2025-07-31 05:16:03 -0500

    Update Civilian.java
    moved from main branch to master

--- src/main/java/com/zombiewar/Civilian.java
+++ src/main/java/com/zombiewar/Civilian.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.zombiewar;
 
-/**
- *
- * @author rachel
- */
-public class Civilian {
-    
+public class Civilian extends Character {
+    public Civilian(String name) {
+        super(name, 100, 10);
+    }
+
+    @Override
+    public void specialAttack(Character target) {
+        target.takeDamage(this.attack);
+        System.out.println(this.name + " whacked " + target.getName() + " for " + this.attack + " damage.");
+    }
 }
