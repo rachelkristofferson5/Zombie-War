c6c84dee8e02e98edcf352ae0dbc896a1158d325
rachelkristofferson5 <kristofr@csp.edu>
2025-08-03 22:22:30 -0500

    Update Civilian.java

--- src/main/java/com/zombiewar/Civilian.java
+++ src/main/java/com/zombiewar/Civilian.java
@@ -4,9 +4,12 @@ public class Civilian extends Survivor {
     public Civilian(String name) {
         super(name, 50, 5);
     }
-    
+
     @Override
     public void attack(Character target) {
         target.takeDamage(5);
+        if (!target.isAlive()) {
+            System.out.println("   " + this.getName() + " killed " + target.getName());
+        }
     }
 }
