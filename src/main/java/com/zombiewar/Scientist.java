29d8dd66f5e322ab0035388dc9c327dd0f59d5f4
rachelkristofferson5 <kristofr@csp.edu>
2025-08-03 22:23:03 -0500

    Update Scientist.java

--- src/main/java/com/zombiewar/Scientist.java
+++ src/main/java/com/zombiewar/Scientist.java
@@ -4,9 +4,12 @@ public class Scientist extends Survivor {
     public Scientist(String name) {
         super(name, 20, 2);
     }
-    
+
     @Override
     public void attack(Character target) {
         target.takeDamage(2);
+        if (!target.isAlive()) {
+            System.out.println("   " + this.getName() + " killed " + target.getName());
+        }
     }
 }
