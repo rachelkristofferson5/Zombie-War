00a4be3070cb8b62924b27d21bd412cea664483c
rachelkristofferson5 <kristofr@csp.edu>
2025-08-03 19:07:32 -0500

    Update Survivor.java
    moved from main into master

--- src/main/java/com/zombiewar/Survivor.java
+++ src/main/java/com/zombiewar/Survivor.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.zombiewar;
 
-/**
- *
- * @author rachel
- */
-public class Survivor {
-    
+public abstract class Survivor extends Character {
+    public Survivor(String name, int health, int attack) {
+        super(name, health, attack);
+    }
+
 }
