47d5e517ab1384809db88b0a572a2cf01fe74ede
rachelkristofferson5 <kristofr@csp.edu>
2025-08-03 22:23:34 -0500

    Update Soldier.java

--- src/main/java/com/zombiewar/Soldier.java
+++ src/main/java/com/zombiewar/Soldier.java
@@ -4,9 +4,12 @@ public class Soldier extends Survivor {
     public Soldier(String name) {
         super(name, 100, 10);
     }
-    
+
     @Override
     public void attack(Character target) {
         target.takeDamage(10);
+        if (!target.isAlive()) {
+            System.out.println("   " + this.getName() + " killed " + target.getName());
+        }
     }
 }
